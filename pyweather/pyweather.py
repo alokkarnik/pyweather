9d484045accb466fe1c14423b43b57c75057a8d0
aloksteel <alokkrnk@gmail.com>
2015-05-23 16:50:39 +0530

    Added error handling

--- pyweather/pyweather.py
+++ pyweather/pyweather.py
@@ -16,23 +16,28 @@ def main():
 	#Pull the current data
 	data = json.load(obj)
 
-
-	title = data['query']['results']['channel']['item']['title']
-	city = data['query']['results']['channel']['location']['city']
-	country = data['query']['results']['channel']['location']['country']
-	date = data['query']['results']['channel']['item']['condition']['date']
-	faren = data['query']['results']['channel']['item']['condition']['temp']
-	weather = data['query']['results']['channel']['item']['condition']['text']
-
-	print "\n"
-	print "Conditions for",city,country
-	print date
-	print "Temperature:",(((int(faren) - 32)) * 5/9 )
-	print "Weather:",weather
-
-	print "\n"
+	try:
+		title = data['query']['results']['channel']['item']['title']
+		city = data['query']['results']['channel']['location']['city']
+		country = data['query']['results']['channel']['location']['country']
+		date = data['query']['results']['channel']['item']['condition']['date']
+		faren = data['query']['results']['channel']['item']['condition']['temp']
+		weather = data['query']['results']['channel']['item']['condition']['text']
 	
+	
+		print "\n"
+		print "Conditions for",city,country
+		print date
+		print "Temperature:",(((int(faren) - 32)) * 5/9 )
+		print "Weather:",weather
 
+		print "\n"
+	
+	except:
+		print "\n"
+		print "Location cannot be found. Please try again..."
+		print "\n"
+		
 if __name__ == '__main__':
 	main()
 	
